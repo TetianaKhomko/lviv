6b187798b25a867d0e848caf194785fdc0ed07ea
Tetiana Khomko <arnelli.tt@gmail.com>
2017-01-23 20:04:33 +0000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+btn.addEventListener('click', makeAjax);
+
+function makeAjax() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var clientData = JSON.parse(xhr.responseText);
+            address.innerHTML = clientData.address;
+            tel.innerHTML = clientData.tel;
+        }
+    }
+    xhr.open('GET', 'data.json', true);
+    xhr.send();
+}
